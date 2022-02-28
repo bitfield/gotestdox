476812c2bc6be11eda88b67262acb9fc06faaec9
John Arundel <john@bitfieldconsulting.com>
2022-02-28 18:59:34 +0000

    Tweak subtest names for testgoxability

--- testgox_test.go
+++ testgox_test.go
@@ -2,80 +2,80 @@ package testgox_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/bitfield/testgox"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestSentenceTurnsTestNamesIntoReadableSentences(t *testing.T) {
+func TestSentence(t *testing.T) {
 	t.Parallel()
 	tcs := []struct {
 		name, input, want string
 	}{
 		{
-			name:  "Normal well-formed test name",
+			name:  "correctly renders a well-formed test name",
 			input: "TestSumCorrectlySumsInputNumbers",
 			want:  "Sum correctly sums input numbers",
 		},
 		{
-			name:  "Initialisms like 'PDF' should be preserved as is",
+			name:  "preserves initialisms such as 'PDF'",
 			input: "TestFooGeneratesValidPDFFile",
 			want:  "Foo generates valid PDF file",
 		},
 		{
-			name:  "More initialisms",
+			name:  "preserves more initialisms",
 			input: "TestToValidUTF8",
 			want:  "To valid UTF 8",
 		},
 		{
-			name:  "Just 'Test' is a valid test name",
+			name:  "knows that just 'Test' is a valid test name",
 			input: "Test",
 			want:  "",
 		},
 		{
-			name:  "Underscores should be treated as word breaks",
+			name:  "treats underscores as word breaks",
 			input: "Test_Foo_GeneratesValidPDFFile",
 			want:  "Foo generates valid PDF file",
 		},
 		{
-			name:  "Single-letter words should be lowercased",
+			name:  "doesn't incorrectly title-case single-letter words",
 			input: "TestFooDoesAThing",
 			want:  "Foo does a thing",
 		},
 		{
-			name:  "Subtest names should be rendered without the slash, and with underscores replaced by spaces",
+			name:  "renders subtest names without the slash, and with underscores replaced by spaces",
 			input: "TestSliceSink/Empty_line_between_two_existing_lines",
 			want:  "Slice sink empty line between two existing lines",
 		},
 		{
-			name:  "Subtest names beginning with a lowercase letter should trigger a word break",
+			name:  "inserts a word break before subtest names beginning with a lowercase letter",
 			input: "TestExec/go_help",
 			want:  "Exec go help",
 		},
 		{
-			name:  "Single-word 'sentences' are okay (though uninformative)",
+			name:  "is okay with test names not in the form of a sentence",
 			input: "TestMatch",
 			want:  "Match",
 		},
 	}
 	for _, tc := range tcs {
-		got := testgox.Sentence(tc.input)
-		if tc.want != got {
-			t.Errorf("%s:\ninput: %q:\nresult: %s", tc.name, tc.input, cmp.Diff(tc.want, got))
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			got := testgox.Sentence(tc.input)
+			if tc.want != got {
+				t.Errorf("%s:\ninput: %q:\nresult: %s", tc.name, tc.input, cmp.Diff(tc.want, got))
+			}
+		})
 	}
 }
 
 func TestParseJSONCorrectlyParsesASingleGoTestJSONOutputLine(t *testing.T) {
 	t.Parallel()
-	time.Sleep(1400 * time.Millisecond)
-	input := `{"Time":"2022-02-28T15:53:43.532326Z","Action":"pass","Package":"github.com/bitfield/script","Test":"TestFindFilesInNonexistentPathReturnsError","Elapsed":0}`
+	input := `{"Time":"2022-02-28T15:53:43.532326Z","Action":"pass","Package":"github.com/bitfield/script","Test":"TestFindFilesInNonexistentPathReturnsError","Elapsed":0.12}`
 	want := testgox.Event{
 		Action:  "pass",
 		Package: "github.com/bitfield/script",
 		Test:    "TestFindFilesInNonexistentPathReturnsError",
-		Elapsed: 0,
+		Elapsed: 0.12,
 	}
 
 	got, err := testgox.ParseJSON(input)
@@ -95,6 +95,20 @@ func TestEventStringFormatsPassEventsWithATick(t *testing.T) {
 		Elapsed: 0.01,
 	}
 	want := " ✔ Foo does x (0.01s)"
+	got := input.String()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestEventStringFormatsFailEventsWithACross(t *testing.T) {
+	t.Parallel()
+	input := testgox.Event{
+		Action:  "fail",
+		Test:    "TestFooDoesX",
+		Elapsed: 0.01,
+	}
+	want := " ✘ Foo does x (0.01s)"
 	got := input.String()
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
